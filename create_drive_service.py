6017eaef3b9c4a5024d0f7c86d78e258c8c52002
MelleJo <141861480+MelleJo@users.noreply.github.com>
2024-10-16 10:03:57 +0200

    fix secrets management

--- create_drive_service.py
+++ create_drive_service.py
@@ -4,7 +4,15 @@ from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
 def create_drive_service():
-    creds = Credentials.from_authorized_user_info(st.secrets["google_credentials"])
+    creds = Credentials.from_authorized_user_info({
+        'client_id': st.secrets['google_oauth']['client_id'],
+        'client_secret': st.secrets['google_oauth']['client_secret'],
+        'refresh_token': st.secrets['google_oauth']['refresh_token']
+    })
+    
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    
     return build('drive', 'v3', credentials=creds)
 
 drive_service = create_drive_service()
